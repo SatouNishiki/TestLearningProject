6afbe0da946cf8b0ecf818c35207020d7ae98dcb
karin_0624 <kalin000624@gmail.com>
2015-11-06 12:27:12 +0900

    ダイスが降るよ

---
+++ TestLearningProject/app/DiceForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TestLearningProject.app
+{
+    partial class DiceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.drawPictureBox = new System.Windows.Forms.PictureBox();
+            this.diceButton = new System.Windows.Forms.Button();
+            this.diceFallTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.drawPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawPictureBox
+            // 
+            this.drawPictureBox.BackColor = System.Drawing.Color.LightCyan;
+            this.drawPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.drawPictureBox.Name = "drawPictureBox";
+            this.drawPictureBox.Size = new System.Drawing.Size(306, 274);
+            this.drawPictureBox.TabIndex = 0;
+            this.drawPictureBox.TabStop = false;
+            // 
+            // diceButton
+            // 
+            this.diceButton.Location = new System.Drawing.Point(324, 263);
+            this.diceButton.Name = "diceButton";
+            this.diceButton.Size = new System.Drawing.Size(75, 23);
+            this.diceButton.TabIndex = 1;
+            this.diceButton.Text = "射出";
+            this.diceButton.UseVisualStyleBackColor = true;
+            this.diceButton.Click += new System.EventHandler(this.diceButton_Click);
+            // 
+            // diceFallTimer
+            // 
+            this.diceFallTimer.Enabled = true;
+            this.diceFallTimer.Tick += new System.EventHandler(this.diceFallTimer_Tick);
+            // 
+            // DiceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 302);
+            this.Controls.Add(this.diceButton);
+            this.Controls.Add(this.drawPictureBox);
+            this.Name = "DiceForm";
+            this.Text = "ダイスを出すよ";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.DiceForm_FormClosed);
+            this.Load += new System.EventHandler(this.DiceForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.drawPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawPictureBox;
+        private System.Windows.Forms.Button diceButton;
+        private System.Windows.Forms.Timer diceFallTimer;
+    }
+}
---
+++ TestLearningProject/dice/Dice.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TestLearningProject.dice
+{
+    /// <summary>
+    /// サイコロを表すクラスです
+    /// </summary>
+    public class Dice
+    {
+        /// <summary>
+        /// ダイスの画像パターン数
+        /// </summary>
+        private const int DicePattern = 3;
+
+        /// <summary>
+        /// ダイス画像
+        /// </summary>
+        private Bitmap picture;
+
+        /// <summary>
+        /// ダイスの画像タイプ
+        /// </summary>
+        public int DiceType { get; set; }
+
+        /// <summary>
+        /// 表示場所
+        /// </summary>
+        public Point Position { get; set; }
+       
+        public Dice()
+        {
+            //ダイスパターンをランダムで生成
+            Random rand = new Random();
+            this.DiceType = rand.Next(DicePattern);
+        }
+
+        /// <summary>
+        /// ダイスタイプに応じた画像を返します
+        /// </summary>
+        /// <returns></returns>
+        public Bitmap GetPicture()
+        {
+            try
+            {
+                if (this.picture == null)
+                {
+                    //同名前空間からタイプに応じたダイス画像を読み込み
+                    picture = new Bitmap(this.GetType(), "dice" + this.DiceType.ToString() + ".png");
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                picture = null;
+            }
+
+            return picture;
+        }
+
+        ~Dice()
+        {
+            this.picture.Dispose();
+        }
+    }
+}
